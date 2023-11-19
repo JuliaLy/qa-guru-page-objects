ca6b02d9fadffb29b867b7e79c01258eb16e99c8
Julia Lysakova <ulyaro@mail.ru>
2023-11-19 03:56:11 +0100

    Added Allure steps

--- src/test/java/tests/RegistrationPageTests.java
+++ src/test/java/tests/RegistrationPageTests.java
@@ -6,38 +6,47 @@ import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import pages.RegistrationPage;
 
+import static io.qameta.allure.Allure.step;
+
 public class RegistrationPageTests extends TestBase{
     RegistrationPage regPage = new RegistrationPage();
     TestData testData=new TestData();
 
 @Test
-@Tag("simple")
+@Tag("remote")
     void successfulRegistrationTest(){
     SelenideLogger.addListener("allure", new AllureSelenide());
-    regPage.openPage()
-            .hideBanner()
-            .setFirstName(testData.randomName)
-            .setLastName(testData.randomLastName)
-            .setUserEmail(testData.randomEmail)
-            .setGender(testData.randomGender)
-            .setUserNumber(testData.randomNumber)
-            .setDateOfBirth(testData.randomDay, testData.randomMonth, testData.randomYear)
-            .setSubject(testData.randomSubject)
-            .selectHobby(testData.randomHobbi)
-            .uploadImage(testData.randomPicture)
-            .setAddress(testData.randomAdress)
-            .setState(testData.randomState)
-            .setCity(testData.randomStateCity)
-            .clickSubmitButton()
-            .checkResultTable(testData.randomName+ " " +testData.randomLastName)
-            .checkResultTable(testData.randomEmail)
-            .checkResultTable(testData.randomGender)
-            .checkResultTable(testData.randomNumber)
-            .checkResultTable(testData.randomDay+ " " +testData.randomMonth+ "," +testData.randomYear)
-            .checkResultTable(testData.randomSubject)
-            .checkResultTable(testData.randomHobbi)
-            .checkResultTable(testData.randomPicture)
-            .checkResultTable(testData.randomAdress)
-            .checkResultTable(testData.randomState+" "+testData.randomStateCity);
+    step("Open form", () -> {
+        regPage.openPage()
+            .hideBanner();
+    });
+    step("Fill form", () -> {
+        regPage.setFirstName(testData.randomName)
+                .setLastName(testData.randomLastName)
+                .setUserEmail(testData.randomEmail)
+                .setGender(testData.randomGender)
+                .setUserNumber(testData.randomNumber)
+                .setDateOfBirth(testData.randomDay, testData.randomMonth, testData.randomYear)
+                .setSubject(testData.randomSubject)
+                .selectHobby(testData.randomHobbi)
+                .uploadImage(testData.randomPicture)
+                .setAddress(testData.randomAdress)
+                .setState(testData.randomState)
+                .setCity(testData.randomStateCity)
+                .clickSubmitButton();
+    });
+    step("Verify results", () -> {
+        regPage.checkResultTable(testData.randomName + " " + testData.randomLastName)
+                .checkResultTable(testData.randomEmail)
+                .checkResultTable(testData.randomGender)
+                .checkResultTable(testData.randomNumber)
+                .checkResultTable(testData.randomDay + " " + testData.randomMonth + "," + testData.randomYear)
+                .checkResultTable(testData.randomSubject)
+                .checkResultTable(testData.randomHobbi)
+                .checkResultTable(testData.randomPicture)
+                .checkResultTable(testData.randomAdress)
+                .checkResultTable(testData.randomState + " " + testData.randomStateCity);
+    });
+    }
 }
-}
+
