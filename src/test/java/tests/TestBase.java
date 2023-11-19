ef1cf0f397da34c2c7cc520bbfc821e476b4ad3b
Julia Lysakova <ulyaro@mail.ru>
2023-11-19 03:35:40 +0100

    Added TestData

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -7,6 +7,10 @@ public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser","chrome");
+        Configuration.browserVersion = System.getProperty("version","100");
+        Configuration.browserSize = System.getProperty("size", "1920X1280");
+        Configuration.remote = System.getProperty("remote", "https://user1:1234@selenoid.autotests.cloud/wd/hub");
         Configuration.pageLoadStrategy = "eager";
         Configuration.browserSize = "1920x1080";
     }
